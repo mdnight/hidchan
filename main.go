5abe42492909a8c9438df2ae1745f6dc9d2b1104
Roman Isaev <goose@riseup.net>
2017-02-16 13:13:59 +0300

    transmission over UDP completed

--- main.go
+++ main.go
@@ -24,32 +24,40 @@ import (
 	"compress/zlib"
 	"errors"
 	"flag"
-	"github.com/mdnight/hidchan/icmptrans/icmptrans"
+	//"github.com/mdnight/hidchan/icmptrans"
+	"github.com/mdnight/hidchan/udptrans"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"path"
 )
 
 var (
 	workMode, defPath, name, compress,
-	destIP *string
+	destIP, srcIP, proto, readFileName *string
+	port *int
 )
 
 func initFlags() {
 	workMode = flag.String("mode", "", "[T]ransmit or [R]eceive mode")
 	defPath = flag.String("o", "./", "Saving directory")
 	name = flag.String("n", "resData", "File name to save. Default: resData")
+	proto = flag.String("proto", "", "[T]CP, [U]DP, [I]CMP")
+	port = flag.Int("port", 0, "TCP/UDP port")
 	compress = flag.String("c", "", "Use compression: [l]zw, [g]zip, [z]lib, [n]one")
-	destIP = flag.String("-d", "", "Destination IPv4")
+	destIP = flag.String("d", "", "Destination IPv4")
+	srcIP = flag.String("s", "", "Source IPv4")
+	readFileName = flag.String("i", "", "File for transmition")
 	flag.Parse()
 }
 
 func main() {
 	initFlags()
 
-	if *destIP == "" {
-		log.Fatal("destination ip was not set")
+	if *proto == "" {
+		log.Println("protocol was not set")
+		os.Exit(0)
 	}
 	switch *workMode {
 	case "R":
@@ -57,13 +65,27 @@ func main() {
 			log.Println("name flag was not set")
 			os.Exit(0)
 		}
-		var data bytes.Buffer
-		err := receive(&data)
+		data, err := receive()
 		if err != nil {
 			log.Fatal(err)
 		}
-		err = ioutil.WriteFile(path.Join(*defPath, *name), data.Bytes(), 0644)
+		err = ioutil.WriteFile(path.Join(*defPath, *name), data, 0644)
 	case "T":
+		if *destIP == "" {
+			log.Println("destination ip was not set")
+			os.Exit(0)
+		}
+
+		buf, err := ioutil.ReadFile(*readFileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = transmit(bytes.NewBuffer(buf))
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	default:
 		log.Println("Select mode ([T]ransmit or [R]eceive)")
 		os.Exit(0)
@@ -71,47 +93,69 @@ func main() {
 
 }
 
-func receive(data *bytes.Buffer) error {
+func receive() ([]byte, error) {
 	if *compress == "" {
-		return errors.New("wrong compression flag value")
+		return []byte{}, errors.New("wrong compression flag value")
 	}
 	//receiving data!!!!!!!!!!!!!!!!!!!!!!!!!!
+	var (
+		recData []byte
+		err     error
+	)
+
+	switch *proto {
+	case "T":
+	case "U":
+		recData, err = udptrans.ReceiveWithLen(port)
+		if err != nil {
+			return []byte{}, err
+		}
+	case "I":
+	default:
+		log.Println("Select correct value -proto ([T]CP, [U]DP, [I]CMP)")
+
+	}
 
 	//decompressing data
 	switch *compress {
 	case "l":
-		var output = make([]byte, data.Len())
-		dec := lzw.NewReader(data, lzw.LSB, 8)
+		dataBuf := bytes.NewBuffer(recData)
+		var output = make([]byte, dataBuf.Len())
+		dec := lzw.NewReader(dataBuf, lzw.LSB, 8)
 		defer dec.Close()
 		_, err := dec.Read(output)
 		if err != nil {
-			return err
+			return []byte{}, err
 		}
-		data.Reset()
-		data.Write(output)
+		dataBuf.Reset()
+		dataBuf.Write(output)
 	case "g":
-		var output = make([]byte, data.Len())
-		dec, err := gzip.NewReader(data)
+		dataBuf := bytes.NewBuffer(recData)
+		var output = make([]byte, dataBuf.Len())
+		dec, err := gzip.NewReader(dataBuf)
 		defer dec.Close()
 		dec.Multistream(false)
 		if err != nil {
-			return err
+			return []byte{}, err
 		}
-		data.Reset()
-		data.Write(output)
+		dataBuf.Reset()
+		dataBuf.Write(output)
 	case "z":
-		var output = make([]byte, data.Len())
-		dec, err := zlib.NewReader(data)
+		dataBuf := bytes.NewBuffer(recData)
+		var output = make([]byte, dataBuf.Len())
+		dec, err := zlib.NewReader(dataBuf)
 		defer dec.Close()
 		if err != nil {
-			return err
+			return []byte{}, err
 		}
-		data.Reset()
-		data.Write(output)
+		dataBuf.Reset()
+		dataBuf.Write(output)
 	case "n":
-		return nil
+
+	default:
+		return []byte{}, errors.New("wrong compression flag value")
 	}
-	return nil
+	return recData, nil
 }
 
 func transmit(data *bytes.Buffer) error {
@@ -136,11 +180,27 @@ func transmit(data *bytes.Buffer) error {
 			return err
 		}
 	case "n":
-		return nil
+		//
 	default:
 		return errors.New("wrong compression flag value")
 	}
 
 	//do transmition
+	switch *proto {
+	case "T":
+	case "U":
+		if *srcIP == "" {
+			return errors.New("source IP was not set")
+		}
+
+		err := udptrans.TransmitWithLen(data.Bytes(), net.ParseIP(*srcIP), net.ParseIP(*destIP), port)
+		if err != nil {
+			return err
+		}
+	case "I":
+	default:
+		log.Println("Select correct value for -proto ([T]CP, [U]DP, [I]CMP)")
+		os.Exit(0)
+	}
 	return nil
 }
--- udptrans/udptrans.go
+++ udptrans/udptrans.go
@@ -1,17 +1,17 @@
 package udptrans
 
 import (
-	"bytes"
 	"encoding/binary"
 	"math/rand"
 	"net"
+	"strconv"
 	"strings"
 	"time"
 )
 
 //TransmitWithLen performs transmitting using field "Length" of UDP datagram
-func TransmitWithLen(data []byte, srcIP, dstIP net.IP, port *uint) error {
-	destAddr, err := net.ResolveUDPAddr("udp4", strings.Join([]string{dstIP.String(), string(*port)}, ":"))
+func TransmitWithLen(data []byte, srcIP, dstIP net.IP, port *int) error {
+	destAddr, err := net.ResolveUDPAddr("udp4", strings.Join([]string{dstIP.String(), strconv.Itoa(*port)}, ":"))
 	if err != nil {
 		return err
 	}
@@ -19,13 +19,11 @@ func TransmitWithLen(data []byte, srcIP, dstIP net.IP, port *uint) error {
 	if err != nil {
 		return err
 	}
-
-	c, err := net.DialUDP("udp", sourceAddr, destAddr)
+	c, err := net.DialUDP("udp4", sourceAddr, destAddr)
 	if err != nil {
 		return err
 	}
 	defer c.Close()
-
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
 
@@ -40,42 +38,40 @@ func TransmitWithLen(data []byte, srcIP, dstIP net.IP, port *uint) error {
 	if err != nil {
 		return err
 	}
-
 	for _, val := range data {
-		size := uint16(val)
+		size := uint(val)
 		size = size << 8
-		tmpBuf := commonBuf[size:]
+		tmpBuf := commonBuf[0:size]
 
-		tmpBuf = func(b []byte, num byte) []byte {
+		tmpBuf = func(b []byte) []byte {
 			tbuf := make([]byte, len(b))
 			for i, v := range b {
-				tbuf[i] = v ^ num
+				tbuf[i] = v ^ byte(r.Intn(0x100))
 			}
 			return tbuf
-		}(tmpBuf, byte(r.Intn(0x100)))
+		}(tmpBuf)
 
 		_, err := c.Write(tmpBuf)
 		if err != nil {
 			return err
 		}
-		time.Sleep(time.Millisecond * 1)
+		time.Sleep(time.Millisecond * 5)
 	}
 	return nil
 }
 
 //ReceiveWithLen performs receiving using field "Length" of UDP datagram
-func ReceiveWithLen(data *bytes.Buffer, port *uint) error {
-	destAddr, err := net.ResolveUDPAddr("udp4", ":"+string(*port))
+func ReceiveWithLen(port *int) ([]byte, error) {
+	destAddr, err := net.ResolveUDPAddr("udp4", ":"+strconv.Itoa(int(*port)))
 	if err != nil {
-		return err
+		return []byte{}, err
 	}
 
 	c, err := net.ListenUDP("udp4", destAddr)
 	if err != nil {
-		return err
+		return []byte{}, err
 	}
 	defer c.Close()
-
 	bs := make([]byte, 4)
 	_, _, err = c.ReadFromUDP(bs)
 
@@ -84,11 +80,9 @@ func ReceiveWithLen(data *bytes.Buffer, port *uint) error {
 	for i := 0; i < len(buf); i++ {
 		n, _, err := c.ReadFromUDP(commonBuf)
 		if err != nil {
-			data = nil
-			return err
+			return []byte{}, err
 		}
-		buf[i] = byte(len(commonBuf[n:]))
+		buf[i] = byte(n >> 8)
 	}
-	data = bytes.NewBuffer(buf)
-	return nil
+	return buf, nil
 }
